28c1f895d401e83f31d3fa6f1646a3689fb46831
po021463 <po021463@gmail.com>
2019-03-18 12:03:15 +0900

    인접함수 수정

--- BeesWars/src/model/GameBoard.java
+++ BeesWars/src/model/GameBoard.java
@@ -1,15 +1,20 @@
 package model;
 
+import java.util.ArrayList;
+
 public class GameBoard {
 	private int[][] board;
+	private int num;
+	private ArrayList<Point> stickedPoint = new ArrayList<Point>();
 	
-	//게임보드 기본생성자
+	//게임보드 기본생성자 : 기본값 7임
 	public GameBoard() {
 		this(7);
 	}
 	
 	//게임보드 생성자
 	public GameBoard(int num) {
+		this.num = num;
 		int idx = 0;
 		int[][] gameBoard = new int[num*2-1][];
 		
@@ -35,7 +40,24 @@ public class GameBoard {
 		}
 	}
 	
-	//게임보드 배열 출력용
+	public void getStickedPoint(int x, int y) {
+		stickedPoint.clear();
+		if(y <= this.num-2) {
+			stickedPoint.add(new Point(x+1,y+1));
+			stickedPoint.add(new Point(x+1,y));
+			if(y!=0) {
+				stickedPoint.add(new Point(x-1,y-1));
+				stickedPoint.add(new Point(x,x-1));
+			}
+			if(y!=board[x].length-1) {
+				stickedPoint.add(new Point(x,x+1));
+				stickedPoint.add(new Point(x-1,y));
+			}
+		}
+	}
+	
+	//게임보드 디버그용
+	@Deprecated
 	public void debugGameBoard() {
 		for(int i=0;i<this.board.length;i++) {
 			for(int j=0;j<this.board[i].length;j++) {
@@ -45,16 +67,22 @@ public class GameBoard {
 		}
 	}
 
+	//입력받은 두 좌표의 값이 보드에서 인접한지 반환함
+	@Deprecated
 	public boolean isStick(int a, int b, int c, int d) {
-		if(Math.abs(a-c)<=1 && Math.abs(b-d)<=1 && (a-c)*(b-d)>=0) {
-			return true;
-		}
 		return false;
 	}
 	
 	//덩어리 분류
-	
 	//덩어리 별 최대최소값 반환하는 함수
 }
 
+class Point {
+	int x;
+	int y;
+	Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
 
